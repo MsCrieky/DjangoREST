e77becaaaf86753fa4368c1ae4e27eb6982f4299
Malin  Sjögren <mail2malins@gmail.com>
2024-03-22 13:52:51 +0000

    Update client origin in settings

--- djangorest/settings.py
+++ djangorest/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import re
 from pathlib import Path
 import os
 import dj_database_url
@@ -61,7 +62,11 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
-ALLOWED_HOSTS = ['mypp5.herokuapp.com', 'localhost', '8000-mscrieky-djangorest-ryinnzfjzzf.ws-eu110.gitpod.io']
+ALLOWED_HOSTS = [
+    os.environ.get('ALLOWED_HOST'),
+    'localhost',
+    '8000-mscrieky-djangorest-ryinnzfjzzf.ws-eu110.gitpod.io',
+    ]
 
 
 # Application definition
@@ -106,12 +111,9 @@ MIDDLEWARE = [
 ]
 
 if 'CLIENT_ORIGIN' in os.environ:
-    CORS_ALLOWED_ORIGINS = [
-        os.environ.get('CLIENT_ORIGIN')
-    ]
-else:
+    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
-        r"^https://.*\.gitpod\.io$",
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
 
 CORS_ALLOW_CREDENTIALS = True
